e8207c4ec1769245a5376b62f6b88bbde6238324
chunhodong <yhms4432@naver.com>
2022-10-04 22:14:36 +0900

    feat: 레이싱게임 출력기능 클래스생성
    출력기능만 담당하는 클래스

---
+++ src/main/java/racingcar/view/RacingGameScreen.java
@@ -0,0 +1,24 @@
+package racingcar.view;
+
+public class RacingGameScreen {
+
+    public void printEntry() {
+        System.out.println("경주 할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void printRetryCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public void printRacingResult(String result) {
+        System.out.println("실행 결과");
+        System.out.println(result);
+    }
+
+    public void printRacingWinner(String winner) {
+        System.out.println("최종 우승자 : ".concat(winner));
+    }
+}
+
+
+
