d06ae9ed5142a8cbaab95d5ce97bc5ad1665fc4b
chunhodong <yhms4432@naver.com>
2022-10-05 23:35:22 +0900

    refactor: RacingCars클래스 검증메소드수정
    쉼표개수 카운팅부분 정적클래스로 분리

--- src/main/java/racingcar/model/RacingCars.java
+++ src/main/java/racingcar/model/RacingCars.java
@@ -1,9 +1,7 @@
 package racingcar.model;
 
 import racingcar.exception.MessageMaker;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import racingcar.utils.StringUtils;
 
 public class RacingCars {
     private static final String INVALID_CARNAMES = "자동차이름은 쉼표를 기준으로 구분할 수 있어야합니다";
@@ -13,23 +11,18 @@ public class RacingCars {
 
         validateNames(carNames);
     }
-    public static void createCars(String carNames) {
-        new RacingCars(carNames);
-
+    public static RacingCars createCars(String carNames) {
+        return new RacingCars(carNames);
     }
 
-    public void validateNames(String carNames){
+    private void validateNames(String carNames){
+        if(carNames == null) throw new IllegalArgumentException(MessageMaker.getMessage(INVALID_CARNAMES));
 
-        Matcher matcher = Pattern.compile(CARNAME_DELIMITER).matcher(carNames);
-        int delimiterCount = 0;
-        while (matcher.find()) {
-            delimiterCount++;
-        }
 
+        int delimiterCount = StringUtils.getCharacterCount(carNames,CARNAME_DELIMITER.charAt(0));
         String[] carNameTokens = carNames.split(CARNAME_DELIMITER);
-
         if(carNameTokens.length != delimiterCount + 1)throw new IllegalArgumentException(MessageMaker.getMessage(INVALID_CARNAMES));
 
-
     }
+
 }
--- src/test/java/racingcar/RacingCarsTest.java
+++ src/test/java/racingcar/RacingCarsTest.java
@@ -5,21 +5,21 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import racingcar.model.RacingCars;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class RacingCarsTest {
 
-
     @Nested
     @DisplayName("자동차컬렉션을 생성하는 create메소드는")
-    class ValidationInput{
+    class Create{
 
         @Test
-        @DisplayName("쉼표를 포함했는데 이름을 구분할 수 없으면")
+        @DisplayName("입력값검증에서 쉼표로 구분한 문자열개수가 쉼표+1개수보다 적으면 예외발생")
         void throw_IllegalArgumentException_when_not_separation_by_commas() {
 
             //given
-            String carNames = "awfwaf";
+            String carNames = "awfwaf,";
 
             //when,then
             assertThatThrownBy(() -> RacingCars.createCars(carNames))
@@ -27,8 +27,10 @@ public class RacingCarsTest {
                     .hasMessageContaining("[ERROR]자동차이름은 쉼표를 기준으로 구분할 수 있어야합니다");
         }
 
-
     }
+
+
+
 
 
 
