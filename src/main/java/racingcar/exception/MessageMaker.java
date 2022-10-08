888d36390a016a3da8c93380d7a446100fb0a6dc
chunhodong <yhms4432@naver.com>
2022-10-08 16:13:44 +0900

    refactor: view패키지 메소드수정
    모든 메소드 static타입으로 수정

--- src/main/java/racingcar/exception/MessageMaker.java
+++ src/main/java/racingcar/exception/MessageMaker.java
@@ -5,7 +5,7 @@ public class MessageMaker {
     private static final String PREFIX = "[ERROR]";
 
     public static String getMessage(String message){
-        return PREFIX.concat(message);
+        return PREFIX.concat(" ").concat(message);
 
     }
 }
--- src/main/java/racingcar/view/GameOutputScreen.java
+++ src/main/java/racingcar/view/GameOutputScreen.java
@@ -1,20 +1,25 @@
 package racingcar.view;
 
 import racingcar.model.car.CarDto;
+import racingcar.model.game.BoardDto;
 
 import java.util.List;
 
 public class GameOutputScreen {
 
-    public void printRacingResult(List<CarDto> carDtos,int count) {
+    public static void printErrorMessage(RuntimeException exception){
+        System.out.println(exception.getMessage());
+    }
+    public static void printRacingResult(BoardDto boardDto) {
         System.out.println("실행 결과");
-        for(int i = 0; i < count; i++){
-           printRacingCarsRecord(carDtos,i);
+        for(int i = 0; i < boardDto.getTryCount(); i++){
+           printRacingCarsRecord(boardDto.getEntrys(),i);
+           System.out.println();
         }
 
     }
 
-    private void printRacingCarsRecord(List<CarDto> carDtos,int index){
+    private static void printRacingCarsRecord(List<CarDto> carDtos,int index){
         for(int i = 0; i < carDtos.size(); i++){
             System.out.print(carDtos.get(i).getName().concat(" : "));
             printRacingCarMoveRecord(carDtos.get(i).getMoveRecords(),index);
@@ -22,22 +27,22 @@ public class GameOutputScreen {
         }
     }
 
-    private void printRacingCarMoveRecord(List<Boolean> moveRecords,int index){
+    private static void printRacingCarMoveRecord(List<Boolean> moveRecords,int index){
 
         for(int i = 0; i <= index; i++){
             printRacingCarMove(moveRecords.get(i));
         }
     }
 
-    private void printRacingCarMove(boolean isMove){
+    private static void printRacingCarMove(boolean isMove){
         if(isMove)
             System.out.print("-");
 
     }
 
-    public void printWinners(String[] winners){
+    public static void printWinners(BoardDto boardDto){
         System.out.print("최종 우승자 : ");
-        System.out.print(String.join(",",winners));
+        System.out.print(String.join(",",boardDto.getWinners()));
     }
 
 
